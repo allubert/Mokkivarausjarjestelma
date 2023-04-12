89cb2bfc5e0bc1255d619caf4cff7e97437388f8
aleksihyvarinen <aleksi.j.hyvarinen@gmail.com>
2023-04-12 15:44:45 +0300

    Napin lisäys.

--- Mokkivarausjarjestelma/Form1.Designer.cs
+++ Mokkivarausjarjestelma/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -50,11 +52,29 @@
             this.textBox1.Text = "Varaa mökki";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(634, 284);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(38, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 122);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Varaa mökki";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -68,5 +88,7 @@
 
         private Button button1;
         private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button2;
     }
 }
