7ff58d4182b62a2803a085dc3009f2ec032af0ef
aleksihyvarinen <aleksi.j.hyvarinen@gmail.com>
2023-04-12 16:43:47 +0300

    buttonien nimeäminen

--- Mokkivarausjarjestelma/Form1.Designer.cs
+++ Mokkivarausjarjestelma/Form1.Designer.cs
@@ -28,84 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.btnToiminta = new System.Windows.Forms.Button();
+            this.btnPalvelu = new System.Windows.Forms.Button();
+            this.btnMokkivaraus = new System.Windows.Forms.Button();
+            this.btnAsiakas = new System.Windows.Forms.Button();
+            this.btnLasku = new System.Windows.Forms.Button();
+            this.lblTittle = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aloitusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toimintaalueidenHallintaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.palveluidenHallintaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mökkivaraustenHallintaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asiakashallintaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.laskujenHallintaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btnToiminta
             // 
-            this.button1.Location = new System.Drawing.Point(336, 224);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(260, 75);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Toiminta-alueiden hallinta";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnToiminta.Location = new System.Drawing.Point(336, 224);
+            this.btnToiminta.Name = "btnToiminta";
+            this.btnToiminta.Size = new System.Drawing.Size(260, 75);
+            this.btnToiminta.TabIndex = 0;
+            this.btnToiminta.Text = "Toiminta-alueiden hallinta";
+            this.btnToiminta.UseVisualStyleBackColor = true;
+            this.btnToiminta.Click += new System.EventHandler(this.btn_Click);
             // 
-            // button2
+            // btnPalvelu
             // 
-            this.button2.Location = new System.Drawing.Point(336, 318);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(260, 75);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Palveluiden hallinta";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnPalvelu.Location = new System.Drawing.Point(336, 318);
+            this.btnPalvelu.Name = "btnPalvelu";
+            this.btnPalvelu.Size = new System.Drawing.Size(260, 75);
+            this.btnPalvelu.TabIndex = 1;
+            this.btnPalvelu.Text = "Palveluiden hallinta";
+            this.btnPalvelu.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnMokkivaraus
             // 
-            this.button3.Location = new System.Drawing.Point(336, 424);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(260, 75);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Mökkivarausten hallinta";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnMokkivaraus.Location = new System.Drawing.Point(336, 421);
+            this.btnMokkivaraus.Name = "btnMokkivaraus";
+            this.btnMokkivaraus.Size = new System.Drawing.Size(260, 75);
+            this.btnMokkivaraus.TabIndex = 2;
+            this.btnMokkivaraus.Text = "Mökkivarausten hallinta";
+            this.btnMokkivaraus.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnAsiakas
             // 
-            this.button4.Location = new System.Drawing.Point(336, 518);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(260, 75);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Asiakashallinta";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnAsiakas.Location = new System.Drawing.Point(336, 518);
+            this.btnAsiakas.Name = "btnAsiakas";
+            this.btnAsiakas.Size = new System.Drawing.Size(260, 75);
+            this.btnAsiakas.TabIndex = 3;
+            this.btnAsiakas.Text = "Asiakashallinta";
+            this.btnAsiakas.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // btnLasku
             // 
-            this.button5.Location = new System.Drawing.Point(336, 616);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(260, 75);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Laskujen hallinta ja seuraus ";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnLasku.Location = new System.Drawing.Point(336, 616);
+            this.btnLasku.Name = "btnLasku";
+            this.btnLasku.Size = new System.Drawing.Size(260, 75);
+            this.btnLasku.TabIndex = 4;
+            this.btnLasku.Text = "Laskujen hallinta";
+            this.btnLasku.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // lblTittle
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("TI-Nspire Sans", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(254, 97);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(413, 98);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Mökkivarausjärjestelmä\r\n    Village Newbies Oy\r\n";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.lblTittle.AutoSize = true;
+            this.lblTittle.Font = new System.Drawing.Font("TI-Nspire Sans", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTittle.Location = new System.Drawing.Point(254, 97);
+            this.lblTittle.Name = "lblTittle";
+            this.lblTittle.Size = new System.Drawing.Size(413, 98);
+            this.lblTittle.TabIndex = 5;
+            this.lblTittle.Text = "Mökkivarausjärjestelmä\r\n    Village Newbies Oy\r\n";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aloitusToolStripMenuItem,
+            this.toimintaalueidenHallintaToolStripMenuItem,
+            this.palveluidenHallintaToolStripMenuItem,
+            this.mökkivaraustenHallintaToolStripMenuItem,
+            this.asiakashallintaToolStripMenuItem,
+            this.laskujenHallintaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(960, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aloitusToolStripMenuItem
+            // 
+            this.aloitusToolStripMenuItem.Name = "aloitusToolStripMenuItem";
+            this.aloitusToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.aloitusToolStripMenuItem.Text = "Aloitus";
+            // 
+            // toimintaalueidenHallintaToolStripMenuItem
+            // 
+            this.toimintaalueidenHallintaToolStripMenuItem.Name = "toimintaalueidenHallintaToolStripMenuItem";
+            this.toimintaalueidenHallintaToolStripMenuItem.Size = new System.Drawing.Size(157, 20);
+            this.toimintaalueidenHallintaToolStripMenuItem.Text = "Toiminta-alueiden hallinta";
+            // 
+            // palveluidenHallintaToolStripMenuItem
+            // 
+            this.palveluidenHallintaToolStripMenuItem.Name = "palveluidenHallintaToolStripMenuItem";
+            this.palveluidenHallintaToolStripMenuItem.Size = new System.Drawing.Size(122, 20);
+            this.palveluidenHallintaToolStripMenuItem.Text = "Palveluiden hallinta";
+            // 
+            // mökkivaraustenHallintaToolStripMenuItem
+            // 
+            this.mökkivaraustenHallintaToolStripMenuItem.Name = "mökkivaraustenHallintaToolStripMenuItem";
+            this.mökkivaraustenHallintaToolStripMenuItem.Size = new System.Drawing.Size(145, 20);
+            this.mökkivaraustenHallintaToolStripMenuItem.Text = "Mökkivarausten hallinta";
+            // 
+            // asiakashallintaToolStripMenuItem
+            // 
+            this.asiakashallintaToolStripMenuItem.Name = "asiakashallintaToolStripMenuItem";
+            this.asiakashallintaToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
+            this.asiakashallintaToolStripMenuItem.Text = "Asiakashallinta";
+            // 
+            // laskujenHallintaToolStripMenuItem
+            // 
+            this.laskujenHallintaToolStripMenuItem.Name = "laskujenHallintaToolStripMenuItem";
+            this.laskujenHallintaToolStripMenuItem.Size = new System.Drawing.Size(107, 20);
+            this.laskujenHallintaToolStripMenuItem.Text = "Laskujen hallinta";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(960, 769);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblTittle);
+            this.Controls.Add(this.btnLasku);
+            this.Controls.Add(this.btnAsiakas);
+            this.Controls.Add(this.btnMokkivaraus);
+            this.Controls.Add(this.btnPalvelu);
+            this.Controls.Add(this.btnToiminta);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Mökkivarausjärjestelmä - Village Newbies Oy";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,11 +175,18 @@
 
         #endregion
 
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Button button4;
-        private Button button5;
-        private Label label1;
+        private Button btnToiminta;
+        private Button btnPalvelu;
+        private Button btnMokkivaraus;
+        private Button btnAsiakas;
+        private Button btnLasku;
+        private Label lblTittle;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem aloitusToolStripMenuItem;
+        private ToolStripMenuItem toimintaalueidenHallintaToolStripMenuItem;
+        private ToolStripMenuItem palveluidenHallintaToolStripMenuItem;
+        private ToolStripMenuItem mökkivaraustenHallintaToolStripMenuItem;
+        private ToolStripMenuItem asiakashallintaToolStripMenuItem;
+        private ToolStripMenuItem laskujenHallintaToolStripMenuItem;
     }
 }
--- Mokkivarausjarjestelma/Form1.cs
+++ Mokkivarausjarjestelma/Form1.cs
@@ -7,14 +7,6 @@ namespace Mokkivarausjarjestelma
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
+        
     }
 }
