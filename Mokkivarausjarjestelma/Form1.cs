59a28337e2d9d57a36634b2e08a7d260f848fdc1
JuhoRissanen5 <juhorissanen5@gmail.com>
2023-04-14 11:12:08 +0300

    vaihto

--- Mokkivarausjarjestelma/Form1.cs
+++ Mokkivarausjarjestelma/Form1.cs
@@ -16,5 +16,10 @@ namespace Mokkivarausjarjestelma
         {
             tbcHallinta.SelectedTab = tbpgToimintaaluehallinta;
         }
+
+        private void btnAsiakas_Click(object sender, EventArgs e)
+        {
+            tbcHallinta.SelectedTab = tbpgAsiakashallinta; 
+        }
     }
 }
