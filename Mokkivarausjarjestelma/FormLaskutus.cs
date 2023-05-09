9cdbf9edae27a704182d0184a770d386d7652a7a
JuhoRissanen5 <juhorissanen5@gmail.com>
2023-05-09 14:19:29 +0300

    muutoksia laskuun

--- Mokkivarausjarjestelma/FormLaskutus.cs
+++ Mokkivarausjarjestelma/FormLaskutus.cs
@@ -58,63 +58,75 @@ namespace Mokkivarausjarjestelma
         }
         private void btnTakaisinAloitusFormiin_Click(object sender, EventArgs e)
         {
-            Form formaloitus = new Form1();
-            this.Hide();
-
-            formaloitus.ShowDialog();
-            this.Close();
+            DialogResult result = MessageBox.Show("Haluatko varmasti siirtyä alkuvalikkoon?", "Varoitus", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if(result == DialogResult.Yes)
+            {
+                Form formaloitus = new Form1();
+                this.Hide();
+                formaloitus.ShowDialog();
+                this.Close();
+            }
+            
         }
 
         private void btnLahetalasku_Click(object sender, EventArgs e)
         {
-            string kohde = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/lasku.pdf";
-            PdfWriter writer = new PdfWriter(kohde);
-            PdfDocument pdf = new PdfDocument(writer);
-            Document lasku = new Document(pdf);
-            PageSize ps = PageSize.A4;
+
+            try
+            {
+                string kohde = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/lasku.pdf";
+                PdfWriter writer = new PdfWriter(kohde);
+                PdfDocument pdf = new PdfDocument(writer);
+                Document lasku = new Document(pdf);
+                PageSize ps = PageSize.A4;
 
 
-            Paragraph header = new Paragraph("Village Newbies laskusi").SetTextAlignment(TextAlignment.CENTER).SetFontSize(20);
-            lasku.Add(header);
-            lasku.Add(new Paragraph("Asiakkaan tiedot").SetFontSize(20));
-            connection.Open();
+                Paragraph header = new Paragraph("Village Newbies laskusi").SetTextAlignment(TextAlignment.CENTER).SetFontSize(20);
+                lasku.Add(header);
+                lasku.Add(new Paragraph("Asiakkaan tiedot").SetFontSize(20));
+                connection.Open();
 
-            string varausID = "SELECT etunimi, sukunimi, email, puhelinnro, lahiosoite, postinro FROM lasku " +
-                       "INNER JOIN varaus on varaus.varaus_id = lasku.varaus_id " +
-                       "INNER JOIN asiakas on asiakas.asiakas_id = varaus.asiakas_id " +
-                       "WHERE lasku.lasku_id=" + dgvLaskutus.CurrentRow.Cells[0].Value.ToString(); ;
+                string varausID = "SELECT etunimi, sukunimi, email, puhelinnro, lahiosoite, postinro FROM lasku " +
+                           "INNER JOIN varaus on varaus.varaus_id = lasku.varaus_id " +
+                           "INNER JOIN asiakas on asiakas.asiakas_id = varaus.asiakas_id " +
+                           "WHERE lasku.lasku_id=" + dgvLaskutus.CurrentRow.Cells[0].Value.ToString(); ;
 
-            string kylysummasta = "SELECT summa FROM lasku WHERE lasku_id=" + dgvLaskutus.CurrentRow.Cells[0].Value.ToString();
+                string kylysummasta = "SELECT summa FROM lasku WHERE lasku_id=" + dgvLaskutus.CurrentRow.Cells[0].Value.ToString();
 
-            MySqlCommand komento1 = new MySqlCommand(kylysummasta, connection);
-            MySqlCommand komento2 = new MySqlCommand(varausID, connection);
-            MySqlDataReader reader = komento2.ExecuteReader();
-            reader.Read();
-            // Laskutettavan tiedot tietokannasta 
-            lasku.Add(new Paragraph("Nimi: " + reader.GetString(0) + " " + reader.GetString(1)).SetFontSize(10));
-            lasku.Add(new Paragraph("Sähköposti: " + reader.GetString(2)).SetFontSize(10));
-            lasku.Add(new Paragraph("Puhelinnumero: " + reader.GetString(3)).SetFontSize(10));
-            lasku.Add(new Paragraph("Lähiosoite ja postinumero: " + reader.GetString(4) + ", " + reader.GetString(5)).SetFontSize(10));
-            lasku.Add(new Paragraph("").SetFontSize(10));
-            reader.Close();
-            MySqlDataReader lukija = komento1.ExecuteReader();
-            lukija.Read();
-            //Laskuttajan tiedot
-            lasku.Add(new Paragraph("Laskuttajan tiedot").SetFontSize(30));
-            lasku.Add(new Paragraph("Nimi: Village Newbies").SetFontSize(10));
-            lasku.Add(new Paragraph("Osoite: Microkatu 1, 70210, Kuopio").SetFontSize(10));
-            lasku.Add(new Paragraph("Tilinumero: FI467XXXXXXXXXXX").SetFontSize(10));
-            lasku.Add(new Paragraph("Hinta: " + lukija.GetDouble(0) + "€").SetFontSize(10));
-            lasku.Add(new Paragraph("Maksuehto: 14pv").SetFontSize(10));
-            lasku.Add(new Paragraph("Viivästyskorko: 10%").SetFontSize(10));
-            lukija.Close();
-            lasku.Close();
-            connection.Close();
+                MySqlCommand komento1 = new MySqlCommand(kylysummasta, connection);
+                MySqlCommand komento2 = new MySqlCommand(varausID, connection);
+                MySqlDataReader reader = komento2.ExecuteReader();
+                reader.Read();
+                // Laskutettavan tiedot tietokannasta 
+                lasku.Add(new Paragraph("Nimi: " + reader.GetString(0) + " " + reader.GetString(1)).SetFontSize(10));
+                lasku.Add(new Paragraph("Sähköposti: " + reader.GetString(2)).SetFontSize(10));
+                lasku.Add(new Paragraph("Puhelinnumero: " + reader.GetString(3)).SetFontSize(10));
+                lasku.Add(new Paragraph("Lähiosoite ja postinumero: " + reader.GetString(4) + ", " + reader.GetString(5)).SetFontSize(10));
+                lasku.Add(new Paragraph("").SetFontSize(10));
+                reader.Close();
+                MySqlDataReader lukija = komento1.ExecuteReader();
+                lukija.Read();
+                //Laskuttajan tiedot
+                lasku.Add(new Paragraph("Laskuttajan tiedot").SetFontSize(30));
+                lasku.Add(new Paragraph("Nimi: Village Newbies").SetFontSize(10));
+                lasku.Add(new Paragraph("Osoite: Microkatu 1, 70210, Kuopio").SetFontSize(10));
+                lasku.Add(new Paragraph("Tilinumero: FI467XXXXXXXXXXX").SetFontSize(10));
+                lasku.Add(new Paragraph("Hinta: " + lukija.GetDouble(0) + "€").SetFontSize(10));
+                lasku.Add(new Paragraph("Maksuehto: 14pv").SetFontSize(10));
+                lasku.Add(new Paragraph("Viivästyskorko: 10%").SetFontSize(10));
+                lukija.Close();
+                lasku.Close();
+                connection.Close();
 
-            MessageBox.Show("Laskun tallentaminen onnistui", "Ilmoitus", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            reader.Close();
+                MessageBox.Show("Laskun tallentaminen onnistui", "Ilmoitus", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                reader.Close();
 
-            paivitalaskudgv();
+                paivitalaskudgv();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Jokin meni pieleen");
+            }
         }
 
         private void btnPoistaLasku_Click(object sender, EventArgs e)
@@ -124,7 +136,7 @@ namespace Mokkivarausjarjestelma
             MySqlCommand komento = new MySqlCommand(poista, connection);
             komento.ExecuteNonQuery();
             paivitalaskudgv();
-            connection.Close(); 
+            connection.Close();
         }
     }
 }
