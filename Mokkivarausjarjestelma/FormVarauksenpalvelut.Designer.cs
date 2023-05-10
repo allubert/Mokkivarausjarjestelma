567e995a98afc7059e6c35eba8e3583e892449ef
Toni Kuura <Toni.Kuura@edu.savonia.fi>
2023-05-10 17:46:37 +0300

    TOILET

--- Mokkivarausjarjestelma/FormVarauksenpalvelut.Designer.cs
+++ Mokkivarausjarjestelma/FormVarauksenpalvelut.Designer.cs
@@ -29,17 +29,23 @@
         private void InitializeComponent()
         {
             this.dgvVarauksenpalvelut = new System.Windows.Forms.DataGridView();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.cmbVarausID = new System.Windows.Forms.ComboBox();
+            this.cmbPalveluID = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.tbLkm = new System.Windows.Forms.TextBox();
+            this.btnVaraukseen = new System.Windows.Forms.Button();
+            this.btnPalveluihin = new System.Windows.Forms.Button();
+            this.btnLaskuihin = new System.Windows.Forms.Button();
+            this.btnLisaa = new System.Windows.Forms.Button();
+            this.btnPoista = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvVarauksenpalvelut)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvVarauksenpalvelut
             // 
+            this.dgvVarauksenpalvelut.BackgroundColor = System.Drawing.Color.DarkSalmon;
             this.dgvVarauksenpalvelut.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvVarauksenpalvelut.Location = new System.Drawing.Point(280, 12);
             this.dgvVarauksenpalvelut.Name = "dgvVarauksenpalvelut";
@@ -47,21 +53,21 @@
             this.dgvVarauksenpalvelut.Size = new System.Drawing.Size(508, 308);
             this.dgvVarauksenpalvelut.TabIndex = 0;
             // 
-            // comboBox1
+            // cmbVarausID
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(198, 48);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(67, 23);
-            this.comboBox1.TabIndex = 1;
+            this.cmbVarausID.FormattingEnabled = true;
+            this.cmbVarausID.Location = new System.Drawing.Point(198, 48);
+            this.cmbVarausID.Name = "cmbVarausID";
+            this.cmbVarausID.Size = new System.Drawing.Size(67, 23);
+            this.cmbVarausID.TabIndex = 1;
             // 
-            // comboBox2
+            // cmbPalveluID
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(198, 103);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(67, 23);
-            this.comboBox2.TabIndex = 2;
+            this.cmbPalveluID.FormattingEnabled = true;
+            this.cmbPalveluID.Location = new System.Drawing.Point(198, 103);
+            this.cmbPalveluID.Name = "cmbPalveluID";
+            this.cmbPalveluID.Size = new System.Drawing.Size(67, 23);
+            this.cmbPalveluID.TabIndex = 2;
             // 
             // label1
             // 
@@ -100,21 +106,83 @@
             this.tbLkm.Name = "tbLkm";
             this.tbLkm.Size = new System.Drawing.Size(253, 23);
             this.tbLkm.TabIndex = 6;
+            this.tbLkm.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbLkm_KeyPress);
+            // 
+            // btnVaraukseen
+            // 
+            this.btnVaraukseen.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.btnVaraukseen.Location = new System.Drawing.Point(12, 315);
+            this.btnVaraukseen.Name = "btnVaraukseen";
+            this.btnVaraukseen.Size = new System.Drawing.Size(180, 23);
+            this.btnVaraukseen.TabIndex = 7;
+            this.btnVaraukseen.Text = "Palaa varausten hallintaan";
+            this.btnVaraukseen.UseVisualStyleBackColor = false;
+            this.btnVaraukseen.Click += new System.EventHandler(this.btnVaraukseen_Click);
+            // 
+            // btnPalveluihin
+            // 
+            this.btnPalveluihin.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.btnPalveluihin.Location = new System.Drawing.Point(12, 351);
+            this.btnPalveluihin.Name = "btnPalveluihin";
+            this.btnPalveluihin.Size = new System.Drawing.Size(180, 23);
+            this.btnPalveluihin.TabIndex = 8;
+            this.btnPalveluihin.Text = "Palaa palveluiden hallintaan";
+            this.btnPalveluihin.UseVisualStyleBackColor = false;
+            this.btnPalveluihin.Click += new System.EventHandler(this.btnPalveluihin_Click);
+            // 
+            // btnLaskuihin
+            // 
+            this.btnLaskuihin.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.btnLaskuihin.Location = new System.Drawing.Point(12, 389);
+            this.btnLaskuihin.Name = "btnLaskuihin";
+            this.btnLaskuihin.Size = new System.Drawing.Size(180, 23);
+            this.btnLaskuihin.TabIndex = 9;
+            this.btnLaskuihin.Text = "Siirry laskujen hallintaan";
+            this.btnLaskuihin.UseVisualStyleBackColor = false;
+            this.btnLaskuihin.Click += new System.EventHandler(this.btnLaskuihin_Click);
+            // 
+            // btnLisaa
+            // 
+            this.btnLisaa.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.btnLisaa.Location = new System.Drawing.Point(12, 235);
+            this.btnLisaa.Name = "btnLisaa";
+            this.btnLisaa.Size = new System.Drawing.Size(75, 23);
+            this.btnLisaa.TabIndex = 10;
+            this.btnLisaa.Text = "Lisää";
+            this.btnLisaa.UseVisualStyleBackColor = false;
+            this.btnLisaa.Click += new System.EventHandler(this.btnLisaa_Click);
+            // 
+            // btnPoista
+            // 
+            this.btnPoista.Location = new System.Drawing.Point(117, 235);
+            this.btnPoista.Name = "btnPoista";
+            this.btnPoista.Size = new System.Drawing.Size(75, 23);
+            this.btnPoista.TabIndex = 11;
+            this.btnPoista.Text = "Poista";
+            this.btnPoista.UseVisualStyleBackColor = true;
+            this.btnPoista.Click += new System.EventHandler(this.btnPoista_Click);
             // 
             // FormVarauksenpalvelut
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSalmon;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPoista);
+            this.Controls.Add(this.btnLisaa);
+            this.Controls.Add(this.btnLaskuihin);
+            this.Controls.Add(this.btnPalveluihin);
+            this.Controls.Add(this.btnVaraukseen);
             this.Controls.Add(this.tbLkm);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cmbPalveluID);
+            this.Controls.Add(this.cmbVarausID);
             this.Controls.Add(this.dgvVarauksenpalvelut);
             this.Name = "FormVarauksenpalvelut";
             this.Text = "FormVarauksenpalvelut";
+            this.Load += new System.EventHandler(this.FormVarauksenpalvelut_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvVarauksenpalvelut)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -124,11 +192,16 @@
         #endregion
 
         private DataGridView dgvVarauksenpalvelut;
-        private ComboBox comboBox1;
-        private ComboBox comboBox2;
+        private ComboBox cmbVarausID;
+        private ComboBox cmbPalveluID;
         private Label label1;
         private Label label2;
         private Label label3;
         private TextBox tbLkm;
+        private Button btnVaraukseen;
+        private Button btnPalveluihin;
+        private Button btnLaskuihin;
+        private Button btnLisaa;
+        private Button btnPoista;
     }
 }
--- Mokkivarausjarjestelma/FormVarauksenpalvelut.cs
+++ Mokkivarausjarjestelma/FormVarauksenpalvelut.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,149 @@ namespace Mokkivarausjarjestelma
         public FormVarauksenpalvelut()
         {
             InitializeComponent();
+
+            Comboboxfill();
+        }
+
+        MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3307;database=vn;username=root;password=Ruutti");
+        MySqlCommand command;
+
+        public void populatedgvVarauspalvelut()
+        {
+            //hae dataa
+            string selectQuery = "SELECT * FROM varauksen_palvelut";
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter(selectQuery, connection);
+            adapter.Fill(table);
+            dgvVarauksenpalvelut.DataSource = table;
+        }
+
+        public void Comboboxfill() //funktio jossa määritetään comboboxien täyttäminen
+        {
+            using (connection)
+            {
+                try
+                {
+                    //täyttää varausID comboboxin! huomioi että varausid pitää luoda, jotta se löytää kyseisen varauksen
+                    string varausidQuery = "SELECT varaus_id FROM varaus";
+                    MySqlDataAdapter alueAdapter = new MySqlDataAdapter(varausidQuery, connection);
+                    DataSet varausDs = new DataSet();
+                    alueAdapter.Fill(varausDs, "varaus");
+
+                    cmbVarausID.DisplayMember = "varaus_id";
+                    cmbVarausID.ValueMember = "varaus_id";
+                    cmbVarausID.DataSource = varausDs.Tables["varaus"];
+
+
+                    //täyttää palveluID comboboxin! huomioi että palveluid pitää luoda, jotta se löytää kyseisen varauksen
+                    string palveluidQuery = "SELECT palvelu_id FROM palvelu";
+                    MySqlDataAdapter palveluAdapter = new MySqlDataAdapter(palveluidQuery, connection);
+                    DataSet palveluDs = new DataSet();
+                    palveluAdapter.Fill(palveluDs, "palvelu");
+
+                    cmbPalveluID.DisplayMember = "palvelu_id";
+                    cmbPalveluID.ValueMember = "palvelu_id";
+                    cmbPalveluID.DataSource = palveluDs.Tables["palvelu"];
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void btnVaraukseen_Click(object sender, EventArgs e)
+        {
+            Form varaus = new FormMokitJaMokkivaraukset();
+            this.Hide();
+
+            varaus.ShowDialog();
+            this.Close();
+        }
+
+        private void btnPalveluihin_Click(object sender, EventArgs e)
+        {
+            Form palvelut = new FormPalveluhallinta();
+            this.Hide();
+
+            palvelut.ShowDialog();
+            this.Close();
+        }
+
+        private void btnLaskuihin_Click(object sender, EventArgs e)
+        {
+            Form laskut = new FormLaskutus();
+            this.Hide();
+
+            laskut.ShowDialog();
+            this.Close();
+        }
+
+        private void btnLisaa_Click(object sender, EventArgs e)
+        {
+            //tarkistaa onko combobokseissa tietoa
+            if (cmbPalveluID.SelectedValue == null || cmbVarausID.SelectedValue == null)
+            {
+                MessageBox.Show("Valitse arvo molemmille combobokseille!");
+                return;
+            }
+            string palveluid = cmbPalveluID.SelectedValue.ToString();
+            string varausid = cmbVarausID.SelectedValue.ToString();
+            int lkm = int.Parse(tbLkm.Text);
+
+            string insertQuery = "INSERT INTO varauksen_palvelut (varaus_id, palvelu_id, lkm) VALUES (@varausid, @palveluid, @lkm)";
+            string selectQuery = "SELECT * FROM varauksen_palvelut WHERE varaus_id = @varausid AND palvelu_id = @palveluid";
+
+            using (connection)
+            {
+                try
+                {
+                    connection.Open();
+                    MySqlCommand insertCommand = new MySqlCommand(insertQuery, connection);
+                    insertCommand.Parameters.AddWithValue("@varausid", varausid);
+                    insertCommand.Parameters.AddWithValue("@palveluid", palveluid);
+                    insertCommand.Parameters.AddWithValue("@lkm", lkm);
+                    insertCommand.ExecuteNonQuery();
+
+                    MySqlCommand selectCommand = new MySqlCommand(selectQuery, connection);
+                    selectCommand.Parameters.AddWithValue("@varausid", varausid);
+                    selectCommand.Parameters.AddWithValue("@palveluid", palveluid);
+
+                    DataTable datatable = new DataTable();
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(selectCommand))
+                    {
+                        adapter.Fill(datatable);
+                        populatedgvVarauspalvelut();
+                    }
+                    dgvVarauksenpalvelut.DataSource = datatable;
+                    populatedgvVarauspalvelut();
+
+                    connection.Close();
+                }
+                catch (Exception ex) 
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+        private void tbLkm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                MessageBox.Show("Syötä haluamasi lukumäärä numeromuodossa!");
+                e.Handled = true;
+            }
+        }
+
+        private void FormVarauksenpalvelut_Load(object sender, EventArgs e)
+        {
+            populatedgvVarauspalvelut();
+        }
+
+        private void btnPoista_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
